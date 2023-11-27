e02c5c0eb9dfec243506c94970d78bd4685f9925
Jaes98 <marcus.jaes@gmail.com>
2023-11-27 10:52:11 +0100

    browserrouter og 2 dummy components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-function App() {
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+
+
+export default function App() {
   return (
     <>
-      <Navbar></Navbar>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/contact" element={<Contact/>}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
