1a40527da93981c01cfcd7250c9f96ea41a6747a
sassnoe <105166994+sassnoe@users.noreply.github.com>
2023-11-27 10:51:11 +0100

    Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-function App() {
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
+import "./App.css";
+
+export default function App() {
   return (
     <>
-      <Navbar></Navbar>
+      <Navbar />
     </>
   );
 }
