47dd11f41d16849ddfcf2df9ee0a993549a544fa
Jaes98 <marcus.jaes@gmail.com>
2023-11-28 11:05:01 +0100

    error messages på ikke udfyldte felter

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -17,14 +17,17 @@ export default function Contact() {
           {...register("firstname", { required: "Feltet er ikke udfyldt" })}
           placeholder="Fornavn"
         ></input>
+        <p>{errors.firstname?.message}</p>
         <input
           {...register("lastname", { required: "Feltet er ikke udfyldt" })}
           placeholder="Efternavn"
         ></input>
+        <p>{errors.lastname?.message}</p>
         <input
           {...register("email", { required: "Feltet er ikke udfyldt" })}
           placeholder="Email"
         ></input>
+        <p>{errors.email?.message}</p>
         <input
           {...register("phone", {
             required: "Feltet er ikke udfyldt",
@@ -32,18 +35,22 @@ export default function Contact() {
           })}
           placeholder="Telefon nummer"
         ></input>
+        <p>{errors.phone?.message}</p>
         <input
           {...register("company", { required: "Feltet er ikke udfyldt" })}
           placeholder="Virksomheds navn"
         ></input>
+        <p>{errors.company?.message}</p>
         <input
           {...register("address", { required: "Feltet er ikke udfyldt" })}
           placeholder="Adresse"
         ></input>
+        <p>{errors.address?.message}</p>
         <input
           {...register("introduction", { required: "Feltet er ikke udfyldt"  })}
           placeholder="Kort introduktion"
         ></input>
+        <p>{errors.introduction?.message}</p>
         <button>Submit</button>
       </form>
     </div>
