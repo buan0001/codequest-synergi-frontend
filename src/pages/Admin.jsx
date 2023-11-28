f343bb71da95005d2f126dab905c1a299f1d315d
Jaes98 <marcus.jaes@gmail.com>
2023-11-28 13:38:29 +0100

    Update Admin.jsx

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,8 +1,43 @@
-export default function Admin() {
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+
+function CenteredLoginForm() {
   return (
-    <>
-      <h1>Admin</h1>
-      <p>Welcome to the admin page!</p>
-    </>
+    <Row className="justify-content-md-center">
+      <Col md={6}>
+        <Form>
+          <Form.Group as={Row} className="mb-3" controlId="formHorizontalEmail">
+            <Form.Label column sm={3}>
+              Email
+            </Form.Label>
+            <Col sm={9}>
+              <Form.Control type="email" placeholder="Email" />
+            </Col>
+          </Form.Group>
+
+          <Form.Group
+            as={Row}
+            className="mb-3"
+            controlId="formHorizontalPassword"
+          >
+            <Form.Label column sm={3}>
+              Password
+            </Form.Label>
+            <Col sm={9}>
+              <Form.Control type="password" placeholder="Password" />
+            </Col>
+          </Form.Group>
+          <Form.Group as={Row} className="mb-3">
+            <Col sm={{ span: 9, offset: 3 }}>
+              <Button type="submit">Sign in</Button>
+            </Col>
+          </Form.Group>
+        </Form>
+      </Col>
+    </Row>
   );
 }
+
+export default CenteredLoginForm;
