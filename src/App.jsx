f985d7586ae3c8a59bd66195d1924426dc651f0b
sassnoe <joakimsassnoe@gmail.com>
2023-11-29 14:08:19 +0100

    Navbar, contactform

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import About from "./pages/About";
 import CV from "./pages/CV";
 import Booksandarticles from "./pages/BooksAndArticles";
 import "./App.css";
+import Theory from "./pages/Theory";
 
 export default function App() {
   return (
@@ -13,15 +14,13 @@ export default function App() {
       <Header />
 
       <Routes>
-        <Route path="/" element={<Home  />}></Route>
+        <Route path="/" element={<Home />}></Route>
         <Route path="/contact" element={<Contact />}></Route>
         <Route path="/about" element={<About />}></Route>
-        <Route path="/cv" element={<CV  />}></Route>
-          <Route
-            path="/booksandarticles"
-            element={<Booksandarticles />}
-          ></Route>
-          <Route path="*" element={<h1>Not Found</h1>}></Route>
+        <Route path="/theory" element={<Theory />}></Route>
+        <Route path="/cv" element={<CV />}></Route>
+        <Route path="/booksandarticles" element={<Booksandarticles />}></Route>
+        <Route path="*" element={<h1>Not Found</h1>}></Route>
       </Routes>
     </>
   );
