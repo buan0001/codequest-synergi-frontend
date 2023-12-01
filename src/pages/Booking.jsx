debf616ea350700ffaebc0458527ffcef5213bb9
sassnoe <105166994+sassnoe@users.noreply.github.com>
2023-12-01 13:51:38 +0100

    Update Booking.jsx

--- src/pages/Booking.jsx
+++ src/pages/Booking.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { registerLocale, setDefaultLocale } from "react-datepicker";
-import da from "date-fns/locale/da";
-registerLocale("da", da);
-setDefaultLocale("da");
 
 export default function Booking() {
   const [startDate, setStartDate] = useState(new Date());
@@ -13,7 +13,49 @@ export default function Booking() {
       <h1>Booking</h1>
       <p>Welcome to the booking page!</p>
 
-      <DatePicker locale="da" selected={startDate} onChange={(date) => setStartDate(date)} />
+      <Form>
+        <Form.Group as={Row} className="mb-3 justify-content-center">
+          <Form.Label column sm={1}>
+            Titel
+          </Form.Label>
+          <Col sm={4}>
+            <Form.Control type="text" placeholder="Titel på booking" />
+          </Col>
+        </Form.Group>
+
+        <Form.Group as={Row} className="mb-3 justify-content-center" controlId="exampleFormControlTextarea1">
+          <Form.Label column sm={1}>
+            Besked
+          </Form.Label>
+          <Col sm={4}>
+            <Form.Control as="textarea" className="bg-light" name="besked" rows={4} />
+          </Col>
+        </Form.Group>
+
+        <Form.Group as={Row} className="mb-3 justify-content-center">
+          <Form.Label column sm={1}>
+            Vælg dato
+          </Form.Label>
+          <Col sm={4}>
+            <DatePicker locale="da" dateFormat="dd/MM/yyyy" closeOnScroll={true} selected={startDate} onChange={(date) => setStartDate(date)} />
+          </Col>
+        </Form.Group>
+
+        <Form.Group as={Row} className="mb-3 justify-content-center">
+          <Form.Label column sm={1}>
+            Vælg slut dato
+          </Form.Label>
+          <Col sm={4}>
+            <DatePicker locale="da" dateFormat="dd/MM/yyyy" closeOnScroll={true} selected={startDate} onChange={(date) => setStartDate(date)} />
+          </Col>
+        </Form.Group>
+
+        <Form.Group className="text-center" controlId="formBasicButton">
+          <Button variant="primary" type="submit" value="Send">
+            Send
+          </Button>
+        </Form.Group>
+      </Form>
     </>
   );
 }
