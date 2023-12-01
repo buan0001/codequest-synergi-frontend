b22b24fba5369cd6d3230b92571296a2bac95e08
LisePetculescu <124042840+LisePetculescu@users.noreply.github.com>
2023-12-01 13:23:48 +0100

    nu med dato og tids picker

--- src/pages/Booking.jsx
+++ src/pages/Booking.jsx
@@ -3,17 +3,27 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { registerLocale, setDefaultLocale } from "react-datepicker";
 import da from "date-fns/locale/da";
+import { setHours, setMinutes } from "date-fns";
 registerLocale("da", da);
 setDefaultLocale("da");
+setHours;
 
 export default function Booking() {
-  const [startDate, setStartDate] = useState(new Date());
+  const [startDate, setStartDate] = useState(setHours(setMinutes(new Date(), 0), 0));
   return (
     <>
       <h1>Booking</h1>
       <p>Welcome to the booking page!</p>
 
-      <DatePicker locale="da" selected={startDate} onChange={(date) => setStartDate(date)} dateFormat="dd/MM/yyyy" />
+      <DatePicker
+        locale="da"
+        selected={startDate}
+        onChange={(date) => setStartDate(date)}
+        showTimeSelect
+        timeFormat="HH:mm"
+        injectTimes={[setHours(setMinutes(new Date(), 1), 0), setHours(setMinutes(new Date(), 5), 12), setHours(setMinutes(new Date(), 59), 23)]}
+        dateFormat="dd/MM/yyyy HH:mm"
+      />
     </>
   );
 }
