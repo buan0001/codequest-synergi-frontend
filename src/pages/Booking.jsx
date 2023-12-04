88bd77bc8a9a5861b54a8238cd21471ce4431342
Lise Freja Jensen Petculescu <124042840+LisePetculescu@users.noreply.github.com>
2023-12-04 19:43:27 +0100

    fooken bs time date picker sorta works

--- src/pages/Booking.jsx
+++ src/pages/Booking.jsx
@@ -7,67 +7,136 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { registerLocale, setDefaultLocale } from "react-datepicker";
 import da from "date-fns/locale/da";
-import { setHours, setMinutes } from "date-fns";
+// import { setHours, setMinutes } from "date-fns";
 registerLocale("da", da);
 setDefaultLocale("da");
 
 export default function Booking() {
-  const [startDate, setStartDate] = useState(setHours(setMinutes(new Date(), 0), 0));
+  const [dateRange, setDateRange] = useState([null, null]);
+  const [startDate, endDate] = dateRange;
+
+  const [startTime, setStartTime] = useState(new Date());
+  const [endTime, setEndTime] = useState(new Date());
+  const [excludedTimes, setExcludedTimes] = useState([]);
+
+  const handleStartTimeChange = (time) => {
+    setStartTime(time);
+    generateExcludedTimes(time);
+  };
+
+  const generateExcludedTimes = (selectedStartTime) => {
+    const excluded = [];
+    const current = new Date(selectedStartTime);
+    const startOfDay = new Date(selectedStartTime);
+    startOfDay.setHours(0, 0, 0);
+
+    while (current >= startOfDay) {
+      excluded.push(new Date(current));
+      current.setMinutes(current.getMinutes() - 15);
+    }
+
+    setExcludedTimes(excluded);
+  };
+
+  const handleEndTimeChange = (time) => {
+    setEndTime(time);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+
+    const startTimeValue = startTime.toLocaleTimeString("da");
+    const endTimeValue = endTime.toLocaleTimeString("da");
+
+    formData.append("bookingTimeStart", startTimeValue);
+    formData.append("bookingTimeEnd", endTimeValue);
+
+    const data = Object.fromEntries(formData.entries());
+    console.log("Form Data:", data);
+  };
+
   return (
     <>
       <h1>Booking</h1>
       <p>Welcome to the booking page!</p>
 
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Form.Group as={Row} className="mb-3 justify-content-center">
-          <Form.Label column sm={1}>
+          <Form.Label column sm={2}>
             Titel
           </Form.Label>
           <Col sm={4}>
-            <Form.Control type="text" placeholder="Titel på booking" />
+            <Form.Control type="text" name="bookingTitle" placeholder="Titel på booking" />
           </Col>
         </Form.Group>
 
         <Form.Group as={Row} className="mb-3 justify-content-center" controlId="exampleFormControlTextarea1">
-          <Form.Label column sm={1}>
+          <Form.Label column sm={2}>
             Besked
           </Form.Label>
           <Col sm={4}>
-            <Form.Control as="textarea" className="bg-light" name="besked" rows={4} />
+            <Form.Control as="textarea" className="bg-light" name="bookingMessage" placeholder="Kort beskrivelse af grunden til du booker.." rows={4} />
           </Col>
         </Form.Group>
 
         <Form.Group as={Row} className="mb-3 justify-content-center">
-          <Form.Label column sm={1}>
-            Vælg dato
+          <Form.Label column sm={2}>
+            Vælg Dato(-er)
           </Form.Label>
           <Col sm={4}>
             <DatePicker
+              className="customDatePicker"
               showIcon
               locale="da"
-              closeOnScroll={true}
-              selected={startDate}
-              onChange={(date) => setStartDate(date)}
-              showTimeSelect
-              timeFormat="HH:mm"
-              injectTimes={[setHours(setMinutes(new Date(), 1), 0), setHours(setMinutes(new Date(), 5), 12), setHours(setMinutes(new Date(), 59), 23)]}
-              dateFormat="dd/MM/yyyy HH:mm"
+              selectsRange={true}
+              startDate={startDate}
+              endDate={endDate}
+              onChange={(update) => {
+                setDateRange(update);
+              }}
+              isClearable={true}
+              withPortal
+              name="bookingDays"
             />
           </Col>
         </Form.Group>
 
         <Form.Group as={Row} className="mb-3 justify-content-center">
-          <Form.Label column sm={1}>
-            Vælg slut dato
+          <Form.Label column sm={2}>
+            Vælg Starttidspunkt
           </Form.Label>
-          <Col sm={4}>
-            <DatePicker locale="da" dateFormat="dd/MM/yyyy" closeOnScroll={true} selected={startDate} onChange={(date) => setStartDate(date)} />
-          </Col>
+          <DatePicker
+            selected={startTime}
+            locale="da"
+            onChange={handleStartTimeChange}
+            showTimeSelect
+            showTimeSelectOnly
+            timeIntervals={15}
+            dateFormat="HH:mm"
+            inline
+            name="bookingTimeStart"
+          />
+          <Form.Label column sm={2}>
+            Vælg Sluttidspunkt
+          </Form.Label>
+          <DatePicker
+            selected={endTime}
+            locale="da"
+            onChange={handleEndTimeChange}
+            showTimeSelect
+            showTimeSelectOnly
+            timeIntervals={15}
+            dateFormat="HH:mm"
+            inline
+            name="bookingTimeEnd"
+            excludeTimes={excludedTimes}
+          />
         </Form.Group>
 
         <Form.Group className="text-center" controlId="formBasicButton">
-          <Button variant="primary" type="submit" value="Send">
-            Send
+          <Button variant="primary" type="submit" value="book">
+            Book
           </Button>
         </Form.Group>
       </Form>
