c655a031c11d0f7a2e9ee7dc465a1a1c8b3aff61
Jaes98 <marcus.jaes@gmail.com>
2023-12-06 17:49:39 +0100

    sort (title & releaseyear) på articles

--- src/components/FetchBooksAndArticles.jsx
+++ src/components/FetchBooksAndArticles.jsx
@@ -4,6 +4,41 @@ export default function FetchComponent() {
   const [data, setData] = useState("");
   const [authorField, setAuthorField] = useState([0]);
   const [newPost, setNewPost] = useState("")
+  const [sortBy, setSortBy] = useState("");
+  const [sortOrder, setSortOrder] = useState("desc");
+
+  const handleSort = (key) => {
+    if (sortBy === key) {
+      setSortOrder(sortOrder === "asc" ? "desc" : "asc");
+    } else {
+      setSortBy(key);
+      setSortOrder("desc");
+    }
+  };
+  const getSortArrow = (key) => {
+    if (sortBy === key) {
+      return sortOrder === "asc" ? "↑" : "↓";
+    }
+    return null;
+  };
+
+  const sortArticles = (articles) => {
+    if (sortBy === "title") {
+      return articles.sort((a, b) =>
+        sortOrder === "asc"
+          ? a.title.localeCompare(b.title)
+          : b.title.localeCompare(a.title)
+      );
+    } else if (sortBy === "releaseYear") {
+      return articles.sort((a, b) =>
+        sortOrder === "asc"
+          ? a.releaseYear - b.releaseYear
+          : b.releaseYear - a.releaseYear
+      );
+    } else {
+      return articles;
+    }
+  };
 
   useEffect(() => {
     async function fetchData() {
@@ -64,110 +99,157 @@ export default function FetchComponent() {
 
   return (
     <div style={{ padding: "10px" }}>
-      <form
-        style={{ display: "flex", padding: "5px", gap: "10px", flexDirection: "column" }}
-        onSubmit={e => {
-          e.preventDefault();
-          handleSubmit(e);
-        }}
-      >
-        <div>
-          <label htmlFor="title">Title</label>
-          <input type="text" name="title" required style={{minWidth:"250px"}} onKeyUp={(e) => e.target.style.width = ((e.target.value.length + 1)) + 'ch'}/>
-        </div>
-        <div>
-          <label htmlFor="">
-            Udgivelsesår
-            <input type="number" name="releaseYear" required />
+      <div>
+        <button onClick={() => handleSort("title")}>
+          Sorter efter titel {getSortArrow("title")}
+        </button>
+        <button onClick={() => handleSort("releaseYear")}>
+          Sorter efter udgivelsesår {getSortArrow("releaseYear")}
+        </button>
+      </div>
+      <div style={{ padding: "10px" }}>
+        <form
+          style={{
+            display: "flex",
+            padding: "5px",
+            gap: "10px",
+            flexDirection: "column",
+          }}
+          onSubmit={(e) => {
+            e.preventDefault();
+            handleSubmit(e);
+          }}
+        >
+          <div>
+            <label htmlFor="title">Title</label>
+            <input
+              type="text"
+              name="title"
+              required
+              style={{ minWidth: "250px" }}
+              onKeyUp={(e) =>
+                (e.target.style.width = e.target.value.length + 1 + "ch")
+              }
+            />
+          </div>
+          <div>
+            <label htmlFor="">
+              Udgivelsesår
+              <input type="number" name="releaseYear" required />
+            </label>
+          </div>
+          <label>
+            Udgiver
+            <input
+              type="text"
+              name="publisher"
+              required
+              style={{ minWidth: "250px" }}
+              onKeyUp={(e) =>
+                (e.target.style.width = e.target.value.length + 1 + "ch")
+              }
+            />
           </label>
-        </div>
-        <label>
-          Udgiver
-          <input type="text" name="publisher" required style={{minWidth:"250px"}} onKeyUp={(e) => e.target.style.width = ((e.target.value.length + 1)) + 'ch'} />
-        </label>
-        <br />
-        <div>
-          {authorField.map((field, index) => {
-            console.log("author field",authorField);
-            return (
-              <div key={field}>
-                <label>
-                  Forfatter {index + 1}:
+          <br />
+          <div>
+            {authorField.map((field, index) => {
+              console.log("author field", authorField);
+              return (
+                <div key={field}>
                   <label>
-                     Fornavn <input type="text" name={"firstName" + index} required />
+                    Forfatter {index + 1}:
+                    <label>
+                      Fornavn{" "}
+                      <input type="text" name={"firstName" + index} required />
+                    </label>
+                    <label htmlFor={"lastName" + index}>
+                      Efternavn{" "}
+                      <input type="text" name={"lastName" + index} required />
+                    </label>
                   </label>
-                  <label htmlFor={"lastName" + index}>
-                     Efternavn <input type="text" name={"lastName" + index} required />
-                  </label>
-                </label>
-              </div>
-            );
-          })}
-          <input
-            type="button"
-            onClick={() => {
-              setAuthorField([...authorField, authorField.length]);
-            }}
-            value={"Add new author"}
-          />
-          <input
-            type="button"
-            onClick={() => {
-              const newField = [...authorField]
-              if(newField.length > 1){newField.pop();setAuthorField(newField )}
-            }}
-            value={"Remove latest author"}
-          />
-        </div>
-        <br />
-        <br />
-        <label>
-          Link til artiklen <input type="link" name="link" style={{minWidth:"250px"}} onKeyUp={(e) => e.target.style.width = ((e.target.value.length + 1)) + 'ch'} />
-          {/* Link til artiklen <input type="link" name="link" style={{width:"50vw"}} /> */}
-        </label>
-        <label>
-          Betalingsartikel? <input type="checkbox" name="pay" />
-        </label>
-        <label>
-          Kort resume <textarea name="resume" />
-        </label>
-        <div>
-          <input type="submit" value={"Opret artikel"} />
-        </div>
-      </form>
-      {/* <div>{missingFields.length == 0 ? "" : "These fields must be filled"}</div>
+                </div>
+              );
+            })}
+            <input
+              type="button"
+              onClick={() => {
+                setAuthorField([...authorField, authorField.length]);
+              }}
+              value={"Add new author"}
+            />
+            <input
+              type="button"
+              onClick={() => {
+                const newField = [...authorField];
+                if (newField.length > 1) {
+                  newField.pop();
+                  setAuthorField(newField);
+                }
+              }}
+              value={"Remove latest author"}
+            />
+          </div>
+          <br />
+          <br />
+          <label>
+            Link til artiklen{" "}
+            <input
+              type="link"
+              name="link"
+              style={{ minWidth: "250px" }}
+              onKeyUp={(e) =>
+                (e.target.style.width = e.target.value.length + 1 + "ch")
+              }
+            />
+            {/* Link til artiklen <input type="link" name="link" style={{width:"50vw"}} /> */}
+          </label>
+          <label>
+            Betalingsartikel? <input type="checkbox" name="pay" />
+          </label>
+          <label>
+            Kort resume <textarea name="resume" />
+          </label>
+          <div>
+            <input type="submit" value={"Opret artikel"} />
+          </div>
+        </form>
+        {/* <div>{missingFields.length == 0 ? "" : "These fields must be filled"}</div>
       <div style={{display:"flex", gap:"10px"}}>{missingFields.length == 0 ? "" : missingFields.map(field =>{
         return <div key={field}>{field}</div>
       })}</div> */}
 
-      {data ? (
-        <div>
-          {data.map(item => (
-            <div key={item._id}>
-              <h3>Title {item.title}</h3>
-              {/* <p>Release {item.release}</p> */}
-              <p>Year {item.releaseYear}</p>
-              <p>Publisher {item.publisher}</p>
-              <div> Authors:
-                {item.authors.map(author => {
-                  return (
-                    <p key={author._id}>
-                      First name: {author.firstName} Last name: {author.lastName}
-                    </p>
-                  );
-                })}
+        {data ? (
+          <div>
+            {sortArticles(data).map((item) => (
+              <div key={item._id}>
+                <h3>Title {item.title}</h3>
+                {/* <p>Release {item.release}</p> */}
+                <p>Year {item.releaseYear}</p>
+                <p>Publisher {item.publisher}</p>
+                <div>
+                  {" "}
+                  Authors:
+                  {item.authors.map((author) => {
+                    return (
+                      <p key={author._id}>
+                        First name: {author.firstName} Last name:{" "}
+                        {author.lastName}
+                      </p>
+                    );
+                  })}
+                </div>
+                {/* <p>{item.author.lastName}</p> */}
+                <a>Link {item.link}</a>
+                {/* pay skal laves om */}
+                <p>Pay {item.pay == false ? "gratis" : "betalt"}</p>
+                <p>Resume {item.resume}</p>
               </div>
-              {/* <p>{item.author.lastName}</p> */}
-              <a>Link {item.link}</a>
-              {/* pay skal laves om */}
-              <p>Pay {item.pay == false ? "gratis" : "betalt"}</p>
-              <p>Resume {item.resume}</p>
-            </div>
-          ))}
-        </div>
-      ) : (
-        <p>Loading...</p> // Loading text hvis data ikke kan hentes
-      )}
+            ))}
+          </div>
+        ) : (
+          <p>Loading...</p> // Placeholder hvis data ikke kan læses eller andet går galt
+        )}
+      </div>
     </div>
   );
 }
