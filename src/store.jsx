e00afa22089abed4339ed03479ff2160599c77c8
Jaes98 <marcus.jaes@gmail.com>
2023-12-07 09:08:57 +0100

    localstorage i redux store

--- src/store.jsx
+++ src/store.jsx
@@ -1,12 +1,54 @@
+// import { configureStore } from "@reduxjs/toolkit";
+// import loginStateReducer from "./features/LoginState";
+
+// console.log("bingchilling");
+
+// const store = configureStore({
+//   reducer: {
+//     loginState: loginStateReducer,
+//   },
+// });
+
+// export default store;
+
 import { configureStore } from "@reduxjs/toolkit";
 import loginStateReducer from "./features/LoginState";
 
-console.log("bingchilling");
+// Funktion til at gemme loginstate i localStorage
+const saveState = (state) => {
+  try {
+    const localStorageState = JSON.stringify(state);
+    localStorage.setItem("loginState", localStorageState);
+  } catch (err) {
+    console.error("Der opstod en fejl ved at gemme i localstorage:", err);
+  }
+};
+
+// Læs loginstate fra localStorage
+const loadState = () => {
+  try {
+    const localStorageState = localStorage.getItem("loginState");
+    if (localStorageState === null) {
+      return undefined
+    }
+    return JSON.parse(localStorageState);
+  } catch (err) {
+    console.error("Der opstod en fejl ved at hente fra localstorage:", err);
+    return undefined;
+  }
+};
 
 const store = configureStore({
   reducer: {
     loginState: loginStateReducer,
   },
+  preloadedState: loadState(), // Læs state fra localStorage
+});
+
+// "Subscribe" til ændringer i state og gem i localStorage
+store.subscribe(() => {
+  const state = store.getState();
+  saveState(state);
 });
 
 export default store;
