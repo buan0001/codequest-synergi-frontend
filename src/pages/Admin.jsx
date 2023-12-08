f80539f3cbba07c6498a7accf1f01f421b767eb3
Jaes98 <marcus.jaes@gmail.com>
2023-12-08 12:24:54 +0100

    update

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -5,15 +5,12 @@ export default function AdminLogin() {
   
   const loggedIn = useSelector((state) => state.loginState.loggedIn);
   console.log('login boolean:', loggedIn);
-  if (loggedIn) {
-    return (
+    return loggedIn ?  (
       <div>
         <h1>Du er nu logget ind som admin</h1>
         <p>Naviger til de andre sider for at redigere.</p>
       </div>
-    );
-  }
-  return (
+    ) : (
     <Login />
   );
 }
