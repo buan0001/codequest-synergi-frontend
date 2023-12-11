350415404f73bce2e99e1228840099f072fac39b
LisePetculescu <124042840+LisePetculescu@users.noreply.github.com>
2023-12-11 12:21:50 +0100

    delete på booking liste

--- src/components/FetchBookings.jsx
+++ src/components/FetchBookings.jsx
@@ -1,8 +1,12 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import tryCatch from "./TryCatch";
+import { Button, Modal } from "react-bootstrap";
 
 export default function FetchComponent() {
   const [data, setData] = useState(null);
+  const [selectedId, setSelectedId] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+  const [selectedItem, setSelectedItem] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -20,12 +24,43 @@ export default function FetchComponent() {
       year: "numeric",
       month: "short",
       day: "numeric"
-      // hour: "numeric",
-      // minute: "numeric",
     };
     return new Date(dateTime).toLocaleString("da-DK", options);
   };
 
+  const handleDelete = async () => {
+    try {
+      const response = await fetch(`http://localhost:3333/booking/${selectedId}`, {
+        method: "DELETE"
+        // Add headers if needed, such as authorization headers
+      });
+
+      if (response.ok) {
+        console.log(`Item with ID ${selectedId} deleted successfully.`);
+        // Update the data after successful deletion (remove the deleted item from the list)
+        setData(data.filter((item) => item.id !== selectedId));
+        // fetchData();
+      } else {
+        console.error("Failed to delete item.");
+      }
+    } catch (error) {
+      console.error("Error occurred while deleting:", error);
+    } finally {
+      setSelectedId(null);
+      setShowModal(false);
+    }
+  };
+
+  const handleNo = () => {
+    setShowModal(false);
+  };
+
+  const handleShowModal = (id) => {
+    const selectedItem = data.find((item) => item._id === id);
+    setSelectedId(id);
+    setShowModal(true);
+    setSelectedItem(selectedItem);
+  };
 
   return (
     <div>
@@ -41,19 +76,23 @@ export default function FetchComponent() {
                 <th>Service</th>
                 <th>Email</th>
                 <th>Telefon nr</th>
+                <th></th>
               </tr>
             </thead>
             <tbody>
-              {data.map((item, index) => (
-                <tr key={index}>
+              {data.map((item) => (
+                <tr key={item._id}>
                   <td>
-                    {item.contactInfo.firstName} {item.contactInfo.lastName}
+                    {item.contactInfo.firstName} {item.contactInfo.lastName}{" "}
                   </td>
                   <td>{formatDateTime(item.appointmentInfo.date)}</td>
                   <td>{item.appointmentInfo.message}</td>
                   <td>{item.appointmentInfo.service}</td>
                   <td>{item.contactInfo.email}</td>
                   <td>{item.contactInfo.phoneNumber}</td>
+                  <td>
+                    <Button onClick={() => handleShowModal(item._id)}>Delete</Button>
+                  </td>
                 </tr>
               ))}
             </tbody>
@@ -62,6 +101,31 @@ export default function FetchComponent() {
       ) : (
         <p>Loading...</p>
       )}
+
+      {/* Modal for confirmation */}
+      <Modal show={showModal} onHide={() => setShowModal(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Du er ved at slette en booking</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {selectedItem && (
+            <div>
+              <p>Er du sikker på du vil slette denne booking?</p>
+              <p>
+                {selectedItem.contactInfo.firstName} {selectedItem.contactInfo.lastName}, {selectedItem.appointmentInfo.service}
+              </p>
+            </div>
+          )}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleNo}>
+            No
+          </Button>
+          <Button variant="primary" onClick={handleDelete}>
+            Yes
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
