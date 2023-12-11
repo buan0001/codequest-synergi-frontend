ccc60953f0d26fdecc3b8e392db411d7f69c6846
buan0001 <bustermikkel@gmail.com>
2023-12-11 13:34:57 +0100

    start blog

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,8 +1,26 @@
-import { useState } from "react"
-import tryCatch from "../components/TryCatch"
+import { useState, useEffect } from "react";
+import tryCatch from "../components/TryCatch";
+
+export default function Blog() {
+  const [posts, setPosts] = useState("");
+
+  useEffect(() => {
+    async function fetchPosts() {
+      const response = await tryCatch("blogPosts");
+      console.log("response", response);
+      if (response) {
+        setPosts(response);
+      }
+    }
+    fetchPosts()
+  }, []); // empty for now
 
 
-export default function Blog(){
-
-    return (<div>HEEEJ</div>)
+  return( <div>{posts.map(entry => {
+    <div>
+      <div>{entry.title}</div>
+      <div>{entry.image}</div>
+      <div>{entry.description}</div>
+    </div>;
+  }) }</div>)
 }
